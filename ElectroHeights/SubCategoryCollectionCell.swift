06f63062235cbb991a4fa79175779a9e0d8b1e30
Varun Rathi <varunrathi28@gmail.com>
2017-09-13 16:31:59 +0530

    SubCategory Cell test commit

---
+++ ElectroHeights/SubCategoryCollectionCell.swift
@@ -0,0 +1,13 @@
+//
+//  SubCategoryCollectionCell.swift
+//  ElectroHeights
+//
+//  Created by Varun Rathi on 13/09/17.
+//  Copyright © 2017 vrat28. All rights reserved.
+//
+
+import UIKit
+
+class SubCategoryCollectionCell: UICollectionViewCell {
+    
+}
